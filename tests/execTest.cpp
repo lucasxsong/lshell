c454895dee687df995d428c4e69bd82ecb174f84
Raymond Kim <rkim216@gmail.com>
2018-11-21 22:12:24 -0800

    checking tests

--- tests/execTest.cpp
+++ tests/execTest.cpp
@@ -12,8 +12,11 @@ TEST(ExecTest, echoTest) {
 }
 
 TEST(ExecTest, errorTest) {
+    cout << "part a" << endl;
     Input i("wrong input");
+    cout << "part b" << endl;
     baseNode* b = i.returnHead();
+    cout << "part c" << endl;
     EXPECT_EQ("Rshell: wrong: command not found\n", b->returnCheck());
 }
 
