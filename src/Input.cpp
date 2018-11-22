dbb999ec70c012c02e8165cc3590cf9fc0905ca9
Raymond Kim <rkim216@gmail.com>
2018-11-21 23:12:28 -0800

    simple change

--- src/Input.cpp
+++ src/Input.cpp
@@ -169,6 +169,21 @@ baseExec* Input::makeExec(std::vector<std::string> exec) {
         b->addArg(exec);
         return b;
     }
+    if (exec.at(0) == "ls") {
+        baseExec* b = new baseExec();
+        b->addArg(exec);
+        return b;
+    }
+    if (exec.at(0) == "mkdir") {
+        baseExec* b = new baseExec();
+        b->addArg(exec);
+        return b;
+    }
+        if (exec.at(0) == "cd") {
+        baseExec* b = new baseExec();
+        b->addArg(exec);
+        return b;
+    }
     else { //error test case
         error* b = new error();
         b->addArg(exec);
