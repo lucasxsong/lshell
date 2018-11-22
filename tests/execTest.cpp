70b23825fb2c928b283deb7f622d371c4aac9d67
Raymond Kim <rkim216@gmail.com>
2018-11-21 21:05:21 -0800

    testing

--- tests/execTest.cpp
+++ tests/execTest.cpp
@@ -7,6 +7,6 @@ using namespace std;
 TEST(ExecTest, echoTest) {
     Input i("echo hi");
     i.parseInput();
-    baseExec* b = i.returnHead();
+    echo* b = i.returnHead();
     EXPECT_EQ("hi ", b->returnArgs());
 }
