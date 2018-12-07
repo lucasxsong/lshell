6155972e54646ca7b72f0e82134b902b965dc388
Lucas Song <lucas7song@gmail.com>
2018-12-06 16:41:38 -0800

    working input

--- src/BaseNode.h
+++ src/BaseNode.h
@@ -233,7 +233,6 @@ class IOverwrite : public Connector { // SYMBOL : <
             std::string file;
             file = rightChild->get_cmd();
             in = open(file.c_str(), O_RDONLY);
-            printf("properly execute");
             return leftChild->execute(in,1);
         }
         std::string returnType() {
